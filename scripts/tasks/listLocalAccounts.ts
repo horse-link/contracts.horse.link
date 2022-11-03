3f0a48647d9f0ee52a788aae241e641d6518bf79
Barry Earsman <barry@earsman.com>
2022-11-03 10:21:17 +1000

    fix: hardhat config

--- scripts/tasks/listLocalAccounts.ts
+++ scripts/tasks/listLocalAccounts.ts
@@ -1,10 +1,11 @@
 import { task } from "hardhat/config";
-import { ethers } from "hardhat";
+import { ethers } from "ethers";
 
 task("accounts", "prints the list of accounts", async (agrs, hre) => {
-  const accounts = await ethers.getSigners();
+  const accounts: ethers.Signer[] = await hre.ethers.getSigners();
   for (const account of accounts) {
+    const address = await account.getAddress();
     const balance = hre.ethers.utils.formatEther(await account.getBalance());
-    console.log(`${account.address} : ${balance} ETH`);
+    console.log(`${address} : ${balance} ETH`);
   }
 });
