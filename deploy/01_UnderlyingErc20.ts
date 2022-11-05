c785c261dcca06af1a73e568b64c5186d290c043
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-11-05 11:56:07 +1000

    chore: linting

--- deploy/01_UnderlyingErc20.ts
+++ deploy/01_UnderlyingErc20.ts
@@ -1,36 +1,43 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
-import {parseEther} from 'ethers/lib/utils';
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { parseEther } from "ethers/lib/utils";
 import "hardhat-deploy";
 import "@nomiclabs/hardhat-ethers";
 
 /*
-* Deploy a test ERC-20 token to be used as an underlying token in the Vault contract
-* This is skipped if the network is not tagged as "test" in hardhat.config.ts
-*/
+ * Deploy a test ERC-20 token to be used as an underlying token in the Vault contract
+ * This is skipped if the network is not tagged as "test" in hardhat.config.ts
+ */
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-	const {deployments, getNamedAccounts} = hre;
-	const {deploy, execute} = deployments;
-	const {deployer} = await getNamedAccounts();
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy, execute } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-	const deployResult = await deploy('Token', {
-		from: deployer,
-		args: ["USDT", "Mock USDT", 18],
-		log: true,
-		autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks,
-        skipIfAlreadyDeployed: true,
-	});
-    if (deployResult?.newlyDeployed) {
-        console.log(
-            `contract Token deployed at ${deployResult.address} using ${deployResult.receipt?.gasUsed ?? "?"} gas`
-        );	
-		await execute('Token', {from: deployer, log: true}, 'mint', deployer, parseEther('1000000000'));
-
-    }
+  const deployResult = await deploy("Token", {
+    from: deployer,
+    args: ["USDT", "Mock USDT", 18],
+    log: true,
+    autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks,
+    skipIfAlreadyDeployed: true
+  });
+  if (deployResult?.newlyDeployed) {
+    console.log(
+      `contract Token deployed at ${deployResult.address} using ${
+        deployResult.receipt?.gasUsed ?? "?"
+      } gas`
+    );
+    await execute(
+      "Token",
+      { from: deployer, log: true },
+      "mint",
+      deployer,
+      parseEther("1000000000")
+    );
+  }
 };
 export default func;
-func.tags = ['vault', 'underlying']; // Deploy if "vault" or "underlying" tags are specified
+func.tags = ["vault", "underlying"]; // Deploy if "vault" or "underlying" tags are specified
 func.skip = async (hre: HardhatRuntimeEnvironment) => {
-	// Skip if this not a test deployment
-	return !hre.network.tags.test;
-}
+  // Skip if this not a test deployment
+  return !hre.network.tags.test;
+};
--- deploy/03_Market.ts
+++ deploy/03_Market.ts
@@ -1,31 +1,33 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
-import {parseEther} from 'ethers/lib/utils';
-import {ethers} from 'hardhat';
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { parseEther } from "ethers/lib/utils";
+import { ethers } from "hardhat";
 import "hardhat-deploy";
 import "@nomiclabs/hardhat-ethers";
 
 /*
-* Deploy a Vault contract with an Underlying ERC-20 token
-*/
+ * Deploy a Vault contract with an Underlying ERC-20 token
+ */
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-	const {deployments, getNamedAccounts, network} = hre;
-	const {deploy, execute} = deployments;
-	const {deployer} = await getNamedAccounts();
-    const oracle = deployer; // TODO: Put oracle address in .env
-    const vaultDeployment = await deployments.get("Vault");
-	const deployResult = await deploy("Market", {
-		from: deployer,
-		args: [vaultDeployment.address, 0, oracle],
-		log: true,
-		autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks,
-        skipIfAlreadyDeployed: true,
-	});
-    if (deployResult?.newlyDeployed) {
-        // Allow the Market to withdraw from the Vault
-        execute("Vault", {from: deployer, log: true}, "approve", [deployResult.address, ethers.constants.MaxUint256]);
-    }
-
+  const { deployments, getNamedAccounts, network } = hre;
+  const { deploy, execute } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const oracle = deployer; // TODO: Put oracle address in .env
+  const vaultDeployment = await deployments.get("Vault");
+  const deployResult = await deploy("Market", {
+    from: deployer,
+    args: [vaultDeployment.address, 0, oracle],
+    log: true,
+    autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks,
+    skipIfAlreadyDeployed: true
+  });
+  if (deployResult?.newlyDeployed) {
+    // Allow the Market to withdraw from the Vault
+    execute("Vault", { from: deployer, log: true }, "approve", [
+      deployResult.address,
+      ethers.constants.MaxUint256
+    ]);
+  }
 };
 export default func;
-func.tags = ['market']; 
+func.tags = ["market"];
