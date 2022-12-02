a2eb1ad67bbe81ea47107851d762eac76bc3db1a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-12-03 07:37:53 +1000

    wip

--- scripts/settle.py
+++ scripts/settle.py
@@ -2,3 +2,7 @@ from web3 import Web3
 
 w3 = Web3(Web3.HTTPProvider('https://eth-goerli.g.alchemy.com/v2/u5vVYvyI5qgU6UXgDlfImbo4UutLzhTH'))
 
+greeter = w3.eth.contract(
+    address=,
+    abi=abi
+)
