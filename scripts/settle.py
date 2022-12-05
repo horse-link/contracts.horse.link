f2a209a62e1a5db7d73adc76fc1f289ff20d6674
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-12-05 11:07:48 +1000

    feat: change to last 2hrs

--- scripts/settle.py
+++ scripts/settle.py
@@ -1,5 +1,6 @@
 from web3 import Web3
 from dotenv import load_dotenv
+from datetime import datetime
 import json
 import os
 import requests
@@ -29,49 +30,56 @@ def get_count(contract):
     return count
 
 
-def settle(contract, index):
-    bet = contract.functions.getBetByIndex(index).call()
-    print(bet)
+def settle(market, index):
+    # bet = contract.functions.getBetByIndex(index).call()
 
-    if bet[3] == False:
-        account_from = {
-            'private_key': os.getenv('PRIVATE_KEY'),
-            'address': '0x155c21c846b68121ca59879B3CCB5194F5Ae115E',
+    # if bet[3] == False:
+    account_from = {
+        'private_key': os.getenv('PRIVATE_KEY'),
+        'address': '0x155c21c846b68121ca59879B3CCB5194F5Ae115E',
+    }
+
+    tx = market.functions.settle(index).buildTransaction(
+        {
+            'from': account_from['address'],
+            'nonce': web3.eth.get_transaction_count(account_from['address']),
         }
+    )
 
-        tx = contract.functions.settle(index).buildTransaction(
-            {
-                'from': account_from['address'],
-                'nonce': web3.eth.get_transaction_count(account_from['address']),
-            }
-        )
+    tx_create = web3.eth.account.sign_transaction(
+        tx, account_from['private_key'])
 
-        tx_create = web3.eth.account.sign_transaction(
-            tx, account_from['private_key'])
+    tx_hash = web3.eth.send_raw_transaction(tx_create.rawTransaction)
+    tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
 
-        tx_hash = web3.eth.send_raw_transaction(tx_create.rawTransaction)
-        tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
-
-        return tx_receipt
+    return tx_receipt
 
 
 def main():
 
     # fetch registry contract address from the api
     markets = get_markets()
+    now = datetime.now().timestamp()
 
     # settle each market
     for market in markets:
         contract = load_market(market['address'])
         count = get_count(contract)
-        start = count - 50 if count > 50 else 0
 
-        # settle last 50
-        for i in range(start, count):
-            print(f"Settling bet {i} for market {market['address']}")
-            
-            tx_receipt = settle(contract, i)
-            print(tx_receipt)
+        ## settle each bet in reverse order
+        for i in range(count - 1, 0, -1):
+            bet = contract.functions.getBetByIndex(index).call()
+                            
+            if bet[2] > now - 60 * 60 * 2:
+                if bet[3] == False:
+                    print(f"Settling bet {i} for market {market['address']}")
+                    
+                    tx_receipt = settle(contract, i)
+                    print(tx_receipt)
+                else:
+                    print(f"Bet {i} for market {market['address']} already settled")
+            else:
+                yield
 
 
 if __name__ == "__main__":
