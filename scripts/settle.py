e90be1b5482555a3276d99f2a22b26431b607032
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-12-06 11:50:24 +1000

    chore: add oracle lookup

--- scripts/settle.py
+++ scripts/settle.py
@@ -17,8 +17,22 @@ def get_markets():
     return data['markets']
 
 
+def get_oracle():
+    response = requests.get('https://horse.link/api/config')
+    data = response.json()
+    return data['addresses']['marketOracle']
+
+
 def load_market(address):
     with open('./artifacts/contracts/Market.sol/Market.json') as f:
+        data = json.load(f)
+        abi = data['abi']
+        contract = web3.eth.contract(address=address, abi=abi)
+        return contract
+
+
+def load_oracle(address):
+    with open('./artifacts/contracts/Oracle.sol/Oracle.json') as f:
         data = json.load(f)
         abi = data['abi']
         contract = web3.eth.contract(address=address, abi=abi)
@@ -28,6 +42,28 @@ def load_market(address):
 def get_count(contract):
     count = contract.functions.getCount().call()
     return count
+
+
+def update_oracle(oracle, index):
+    account_from = {
+        'private_key': os.getenv('PRIVATE_KEY'),
+        'address': '0x155c21c846b68121ca59879B3CCB5194F5Ae115E',
+    }
+
+    tx = oracle.functions.setResult(index).buildTransaction(
+        {
+            'from': account_from['address'],
+            'nonce': web3.eth.get_transaction_count(account_from['address']),
+        }
+    )
+
+    tx_create = web3.eth.account.sign_transaction(
+        tx, account_from['private_key'])
+
+    tx_hash = web3.eth.send_raw_transaction(tx_create.rawTransaction)
+    tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+
+    return tx_receipt
 
 
 def settle(market, index):
@@ -55,17 +91,19 @@ def settle(market, index):
 def main():
     # fetch registry contract address from the api
     markets = get_markets()
+    # oracle = get_oracle()
     now = datetime.now().timestamp()
     print(f"Current Time: {now}")
 
     # settle each market
     for market in markets:
-        contract = load_market(market['address'])
+        market = load_market(market['address'])
+        # oracle = load_oracle()
         count = get_count(contract)
 
         # settle each bet in reverse order
         for i in range(count - 1, 0, -1):
-            bet = contract.functions.getBetByIndex(i).call()
+            bet = market.functions.getBetByIndex(i).call()
 
             if bet[2] > now - 60 * 60 * 2:
 
