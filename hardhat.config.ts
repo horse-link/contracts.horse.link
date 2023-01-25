1bcdbbdaad58f3226b194cf81c15e71bc77ba064
Barry Earsman <barry@earsman.com>
2023-01-25 10:46:14 +1000

    chore: remove unlimitedContractSize

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -35,7 +35,7 @@ export default {
 		hardhat: {
 			chainId: 1337,
 			saveDeployment: true,
-			allowUnlimitedContractSize: true,
+			allowUnlimitedContractSize: false,
 			tags: ["local", "testing"]
 		},
 		localhost: {
