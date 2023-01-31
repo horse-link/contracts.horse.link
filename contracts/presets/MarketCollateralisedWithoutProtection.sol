a9914b2181468c193d8583b7828d3e1f46e190b5
Barry Earsman <barry@earsman.com>
2023-01-31 11:44:38 +1000

    chore: use "collateralised"

---
+++ contracts/presets/MarketCollateralisedWithoutProtection.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.15;
+
+import "../MarketWithoutProtection.sol";
+import "../MarketCollateralised.sol";
+import "../Market.sol";
+
+contract MarketCollateralisedWithoutProtection is MarketCollateralised {
+
+    constructor(
+        IVault vault,
+        uint8 fee,
+        uint8 timeoutDays,
+        address oracle
+    ) Market(vault, fee, timeoutDays, oracle) {
+    }
+
+    function _getAdjustedOdds(
+		uint256 wager,
+		uint256 odds,
+		uint256 pool
+	) internal pure override returns (uint256) {
+		return odds;
+	}
+
+}
