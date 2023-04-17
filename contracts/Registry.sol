3f050dfb998053f59de29ee72f85c44d148240c6
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-04-17 18:53:53 +1000

    wip

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.15;
 
+import "./I6224.sol";
 import "./IMarket.sol";
 import "./IVault.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
@@ -44,33 +45,54 @@ contract Registry is IContractsRegistry {
     }
 
     function upgradeContract(string memory name, address newImplementation) external {
+        require(_contracts[name] != address(0), "upgradeContract: Contract not found");
+        require(newImplementation != address(0), "upgradeContract: New implementation address is invalid");
+        
+        // Old contract address
+        address contractAddress = _contracts[name];
         require(
-            IOwnable(_contract).getOwner() == msg.sender,
+            IOwnable(contractAddress).getOwner() == msg.sender,
             "upgradeContract: Only owner can upgrade contracts"
         );
 
-        require(_contracts[name] != address(0), "upgradeContract: Contract not found");
-        require(newImplementation != address(0), "upgradeContract: New implementation address is invalid");
+        address market = IVault(contractAddress).getMarket();
+        // _removeMarket(index, market);
+
         _contracts[name] = newImplementation;
         emit ContractUpgraded(name, newImplementation);
     }
 
-    function addVault(address vault) external onlyTokenHolders {
-        _addVault(vault);
+    // function addVault(address vault) external onlyTokenHolders {
+    //     _addVault(vault);
+    // }
+
+    function addContract(string memory name, address contractAddress) external {
+        require(_contracts[name] == address(0), "addContract: Contract already added");
+        require(contractAddress != address(0), "addContract: Contract address is invalid");
+
+        assert(markets.length == vaults.length);
+
+        address market = IVault(contractAddress).getMarket();
+        require(market != address(0), "addContract: Market has not been set");
+
+        _contracts[name] = contractAddress;
+        _addVault(contractAddress);
+        _addMarket(market);
+
+        emit AddedContract(name, contractAddress, false);
     }
 
     function _addVault(address vault) private {
-        require(vault != address(0), "addVault: Vault address is invalid");
         address assetAddress = IVault(vault).asset();
         require(_vaultByAsset[assetAddress] == address(0), "addVault: Vault with this asset token already added");
 
         vaults.push(vault);
         _vaultByAsset[assetAddress] = vault; 
 
-        emit VaultAdded(vault);
+        // emit VaultAdded(vault);
     }
 
-    function removeVault(uint256 index, address vault) external onlyVaultOwner(vault) {
+    function _removeVault(uint256 index, address vault) private {
         if (index >= vaults.length) return;
 
         for (uint256 i = index; i < vaults.length - 1; i++){
@@ -80,20 +102,20 @@ contract Registry is IContractsRegistry {
         vaults.pop();
 
         delete _vaultByAsset[vault];
-        emit VaultRemoved(vault);
+        // emit RemovedContract();
     }
 
-    function addMarket(address market) external onlyTokenHolders {
+    function _addMarket(address market) private {
         require(
             _markets[market] == address(0),
             "addMarket: Market already added"
         );
         _markets[market] = market;
         markets.push(market);
-        emit MarketAdded(market);
+        // emit AddedContract(market);
     }
 
-    function removeMarket(uint256 index, address market) external onlyContractOwner(market) {
+    function _removeMarket(uint256 index, address market) private {
         if (index >= markets.length) return;
 
         for (uint256 i = index; i < markets.length - 1; i++){
@@ -103,7 +125,7 @@ contract Registry is IContractsRegistry {
         markets.pop();
 
         delete _markets[market];
-        emit MarketRemoved(market);
+        // emit MarketRemoved(market);
     }
 
     function setThreshold(uint256 threshold) external onlyOwner {
