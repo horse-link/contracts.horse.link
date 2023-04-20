56166a58de2b49a87477b8544f19202068b2b7e3
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-04-21 09:48:02 +1000

    feat: refactor

--- contracts/IVault.sol
+++ contracts/IVault.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.15;
 
-import "@openzeppelin/contracts/interfaces/IERC4626.sol";
-
 interface IVault {
     function getMarket() external view returns (address);
     function getMarketAllowance() external view returns (uint256);
