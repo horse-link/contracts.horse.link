11cd9788e1eafb677ad24ea17642611abddd674d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-05-25 15:49:15 +1000

    chore: update tests

--- tests/endtoend.test.ts
+++ tests/endtoend.test.ts
@@ -16,8 +16,7 @@ import {
 	makeMarketId,
 	makePropositionId,
 	signBackMessage,
-	signSetResultMessage,
-	signSetScratchedMessage
+	signSetResultMessage
 } from "./utils";
 import { formatBytes16String } from "../scripts/utils";
 
@@ -32,8 +31,9 @@ describe("End to End", () => {
 	let owner: SignerWithAddress;
 	let alice: SignerWithAddress;
 	let bob: SignerWithAddress;
+	let carol: SignerWithAddress;
+	let dean: SignerWithAddress;
 	let market_owner: SignerWithAddress;
-	let whale: SignerWithAddress;
 	let oracleSigner: SignerWithAddress;
 
 	const USDT_DECIMALS = 6;
@@ -46,7 +46,7 @@ describe("End to End", () => {
 	const NFT_BASE_URI = "https://example.org/";
 
 	beforeEach(async () => {
-		[owner, alice, bob, market_owner] = await ethers.getSigners();
+		[owner, alice, bob, carol, dean, market_owner] = await ethers.getSigners();
 		oracleSigner = owner;
 		const fixture = await deployments.fixture([
 			"underlying",
@@ -83,14 +83,27 @@ describe("End to End", () => {
 			owner.address,
 			ethers.utils.parseUnits("100000000000000", USDT_DECIMALS)
 		);
+
 		await underlying.transfer(
 			alice.address,
-			ethers.utils.parseUnits("2000000000", USDT_DECIMALS) // $2k
+			ethers.utils.parseUnits("2000000000", USDT_DECIMALS)
 		);
+
 		await underlying.transfer(
 			bob.address,
-			ethers.utils.parseUnits("2000000000", USDT_DECIMALS) // $2k
+			ethers.utils.parseUnits("2000000000", USDT_DECIMALS)
 		);
+
+		await underlying.transfer(
+			carol.address,
+			ethers.utils.parseUnits("1000", USDT_DECIMALS) // $1k
+		);
+
+		await underlying.transfer(
+			dean.address,
+			ethers.utils.parseUnits("1000", USDT_DECIMALS) // $1k
+		);
+
 		// await underlying.transfer(
 		// 	carol.address,
 		// 	ethers.utils.parseUnits("1000", USDT_DECIMALS)
@@ -128,12 +141,16 @@ describe("End to End", () => {
 			oracle.address,
 			NFT_BASE_URI
 		];
-		market = (await marketFactory.connect(owner).deploy(...args)) as Market;
+		market = (await marketFactory
+			.connect(market_owner)
+			.deploy(...args)) as Market;
 		await market.deployed();
 
-		expect(await market.owner()).to.eq(owner.address);
+		expect(await market.owner()).to.eq(market_owner.address);
 
-		await vault.setMarket(market.address, ethers.constants.MaxUint256, 107000);
+		await vault.setMarket(market.address, ethers.constants.MaxUint256, 107000); // 1.07x or 7% fee
+
+		// Approve vault to spend USDT for investors
 		await underlying
 			.connect(alice)
 			.approve(vault.address, ethers.constants.MaxUint256);
@@ -148,11 +165,12 @@ describe("End to End", () => {
 		// 	.connect(whale)
 		// 	.approve(vault.address, ethers.constants.MaxUint256);
 
+		// Approve market to spend USDT for punters
 		await underlying
-			.connect(alice)
+			.connect(carol)
 			.approve(market.address, ethers.constants.MaxUint256);
 		await underlying
-			.connect(bob)
+			.connect(dean)
 			.approve(market.address, ethers.constants.MaxUint256);
 
 		// await underlying
@@ -175,14 +193,40 @@ describe("End to End", () => {
 		// expect(
 		// 	await market.getPotentialPayout(propositionId, marketId, wager, odds)
 		// ).to.equal(wager);
+
+		const carolBalance = await underlying.balanceOf(carol.address);
+		expect(carolBalance).to.equal(
+			ethers.utils.parseUnits("1000", USDT_DECIMALS)
+		);
 	});
 
+	const checkMarketTotals = async (
+		expectedInPlay: BigNumber,
+		expectedInPlayCount: number,
+		expectedExposure: BigNumber
+	) => {
+		// Markets should be empty
+		const inPlay = await market.getTotalInPlay();
+		expect(inPlay).to.equal(expectedInPlay);
+
+		const inPlayCount = await market.getInPlayCount();
+		expect(inPlayCount).to.equal(expectedInPlayCount);
+
+		const exposure = await market.getTotalExposure();
+		expect(exposure).to.equal(expectedExposure);
+	};
+
 	it.only("Should do end to end test", async () => {
+		let marketOwnerBalance = await underlying.balanceOf(owner.address);
+		console.log("marketOwnerBalance", marketOwnerBalance.toString());
+
 		// Deposit 1000 USDT from alice
+		const ZERO = BigNumber.from(0);
 		const FIFTY = ethers.utils.parseUnits("50", USDT_DECIMALS);
 		const ONE_HUNDRED = ethers.utils.parseUnits("100", USDT_DECIMALS);
 		const TWO_HUNDRED = ethers.utils.parseUnits("200", USDT_DECIMALS);
 		const FOUR_HUNDRED = ethers.utils.parseUnits("400", USDT_DECIMALS);
+		const FIVE_HUNDRED = ethers.utils.parseUnits("500", USDT_DECIMALS);
 		const ONE_THOUSAND = ethers.utils.parseUnits("1000", USDT_DECIMALS);
 		const TEN_THOUSAND = ethers.utils.parseUnits("10000", USDT_DECIMALS);
 
@@ -204,13 +248,13 @@ describe("End to End", () => {
 		await vault.connect(alice).deposit(TEN_THOUSAND, alice.address);
 
 		// Markets should be empty
-		let inPlay = await market.getTotalInPlay();
+		const inPlay = await market.getTotalInPlay();
 		expect(inPlay).to.equal(0, "Should have $0 USDT in play");
 
-		let inPlayCount = await market.getInPlayCount();
+		const inPlayCount = await market.getInPlayCount();
 		expect(inPlayCount).to.equal(0, "Should have 0 bets in play");
 
-		let exposure = await market.getTotalExposure();
+		const exposure = await market.getTotalExposure();
 		expect(exposure).to.equal(0, "Should have $0 USDT exposure");
 
 		// Place a $100 bet from bettor 1 on proposition / horse 1
@@ -220,70 +264,94 @@ describe("End to End", () => {
 
 		const marketId = makeMarketId(new Date(), "ABC", "1");
 
+		// ###################
+		// # Place bet 1
+		// ###################
 		const propositionId_1 = makePropositionId(marketId, 1);
-		const nonce_1 = "1";
+		const nonce_0 = "0";
 
-		const signature_1 = await signBackMessage(
-			nonce_1,
+		const signature_0 = await signBackMessage(
+			nonce_0,
 			marketId,
 			propositionId_1,
 			ethers.utils.parseUnits("5", ODDS_DECIMALS), // odds,
 			close,
 			end,
-			owner
+			market_owner
 		);
 
-		await market.connect(bob).back(
+		await market.connect(carol).back(
 			constructBet(
-				formatBytes16String(nonce_1),
+				formatBytes16String(nonce_0),
 				formatBytes16String(propositionId_1),
 				formatBytes16String(marketId),
 				ONE_HUNDRED, // wager
 				ethers.utils.parseUnits("5", ODDS_DECIMALS), // odds,
 				close,
 				end,
-				signature_1
+				signature_0
 			)
 		);
+
+		const bet_0 = await market.getBetByIndex(0);
+		const betAmount = bet_0[0];
+		const betPayout = bet_0[1];
+
+		expect(betAmount).to.equal(ONE_HUNDRED, "Should have $100 USDT bet");
+		expect(betPayout).to.equal(FIVE_HUNDRED, "Should have $500 USDT payout");
+
+		let carolBalance = await underlying.balanceOf(carol.address);
+		expect(carolBalance).to.equal(
+			ethers.utils.parseUnits("900", USDT_DECIMALS)
+		);
+
+		let marketBalance = await underlying.balanceOf(market.address);
+		expect(marketBalance).to.equal(FIVE_HUNDRED);
 
 		// $100 market total
 		expect(await market.getMarketTotal(formatBytes16String(marketId))).to.equal(
 			ONE_HUNDRED
 		);
 
-		inPlay = await market.getTotalInPlay();
-		expect(inPlay).to.equal(ONE_HUNDRED, "Should have $100 USDT in play");
+		// inPlay = await market.getTotalInPlay();
+		// expect(inPlay).to.equal(ONE_HUNDRED, "Should have $100 USDT in play");
 
-		inPlayCount = await market.getInPlayCount();
-		expect(inPlayCount).to.equal(1, "Should have 1 bets in play");
+		// inPlayCount = await market.getInPlayCount();
+		// expect(inPlayCount).to.equal(1, "Should have 1 bets in play");
 
-		exposure = await market.getTotalExposure();
-		expect(exposure).to.equal(FOUR_HUNDRED, "Should have $400 USDT exposure");
+		// exposure = await market.getTotalExposure();
+		// expect(exposure).to.equal(FOUR_HUNDRED, "Should have $400 USDT exposure");
 
+		// inplay, inplayCount, exposure
+		await checkMarketTotals(ONE_HUNDRED, 1, FOUR_HUNDRED);
+
+		// ###################
+		// # Place bet 2
+		// ###################
 		// Place a $50 bet from bettor 2 on proposition / horse 2
 		const propositionId_2 = makePropositionId(marketId, 1);
-		const nonce_2 = "2";
+		const nonce_1 = "1";
 
-		const signature_2 = await signBackMessage(
-			nonce_2,
+		const signature_1 = await signBackMessage(
+			nonce_1,
 			marketId,
 			propositionId_2,
 			ethers.utils.parseUnits("2", ODDS_DECIMALS), // odds,
 			close,
 			end,
-			owner
+			market_owner
 		);
 
-		await market.connect(bob).back(
+		await market.connect(dean).back(
 			constructBet(
-				formatBytes16String(nonce_2),
+				formatBytes16String(nonce_1),
 				formatBytes16String(propositionId_2),
 				formatBytes16String(marketId),
 				ONE_HUNDRED, // wager
 				ethers.utils.parseUnits("2", ODDS_DECIMALS), // odds,
 				close,
 				end,
-				signature_2
+				signature_1
 			)
 		);
 
@@ -292,11 +360,14 @@ describe("End to End", () => {
 			TWO_HUNDRED
 		);
 
-		inPlayCount = await market.getInPlayCount();
-		expect(inPlayCount).to.equal(2, "Should have 2 bets in play");
+		// inPlayCount = await market.getInPlayCount();
+		// expect(inPlayCount).to.equal(2, "Should have 2 bets in play");
 
 		// exposure = await market.getTotalExposure();
-		// expect(exposure).to.equal(FOUR_HUNDRED, "Should have $400 USDT exposure");
+		// expect(exposure).to.equal(FIVE_HUNDRED, "Should have $500 USDT exposure");
+
+		// inplay, inplayCount, exposure
+		await checkMarketTotals(TWO_HUNDRED, 2, FIVE_HUNDRED);
 
 		// Close the market
 		await hre.network.provider.request({
@@ -317,15 +388,52 @@ describe("End to End", () => {
 			result_signature
 		);
 
-		// Settle the market
-		await market.settleMarket(formatBytes16String(marketId));
+		// // Settle the market
+		// await market.settleMarket(formatBytes16String(marketId));
 
-		inPlayCount = await market.getInPlayCount();
-		expect(inPlayCount).to.equal(0);
+		// Settle the winner, no one else gets paid
+		await market.settle(0); // WIN
+
+		// inplay, inplayCount, exposure
+		await checkMarketTotals(ONE_HUNDRED, 1, ONE_HUNDRED);
+
+		// inPlayCount = await market.getInPlayCount();
+		// expect(inPlayCount).to.equal(1);
+
+		marketOwnerBalance = await underlying.balanceOf(market_owner.address);
+		expect(marketOwnerBalance).to.equal(
+			0,
+			"Should have $0 USDT in market owner account"
+		);
+
+		carolBalance = await underlying.balanceOf(carol.address);
+		expect(carolBalance).to.equal(
+			ethers.utils.parseUnits("1400", USDT_DECIMALS)
+		);
+
+		marketBalance = await underlying.balanceOf(market.address);
+		expect(marketBalance).to.equal(TWO_HUNDRED);
+
+		// Settle winning bet, bet index 1
+		await market.settle(1);
+
+		// inplay, inplayCount, exposure
+		await checkMarketTotals(ZERO, 0, ZERO);
+
+		marketBalance = await underlying.balanceOf(market.address);
+		expect(marketBalance).to.equal(0);
+
+		// vaultBalance = await underlying.balanceOf(vault.address);
+		// expect(vaultBalance).to.equal(0);
 
 		// Check the vault balance
 
 		// Check market owner balance
-		const marketOwnerBalance = await underlying.balanceOf(market_owner.address);
+		// Should have the profits form the loosing bet less the 7% interest
+		// marketOwnerBalance = await underlying.balanceOf(market_owner.address);
+		// expect(marketOwnerBalance).to.equal(
+		// 	ethers.utils.parseUnits("93", USDT_DECIMALS),
+		// 	"Should have $93 USDT in market owner account"
+		// );
 	});
 });
