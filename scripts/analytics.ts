fc09488dbda1ad825f84551e2272bd5de68ed31e
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-11-16 14:37:02 +1000

    feat: begin script

---
+++ scripts/analytics.ts
@@ -0,0 +1,72 @@
+import dotenv from "dotenv";
+import fs from "fs";
+import {
+	loadOracle,
+	hydrateMarketId,
+	loadMarket,
+	bytes16HexToString,
+	setProvider,
+	setAxiosClient,
+	axiosClient,
+	formatBytes16String
+} from "./utils";
+import { ERC20__factory } from "../build/typechain";
+import { ethers } from "ethers";
+
+dotenv.config();
+
+// const hexZero: Bytes16 = "0x00000000000000000000000000000000";
+
+export type Bytes16 = string;
+
+export async function main() {
+	const deploymentName = "prod_arbitrum";
+	const { chainId, baseApiUrl, privateKeyEnvVar, providerUrlEnvVar } =
+		JSON.parse(fs.readFileSync(`./config_${deploymentName}.json`).toString());
+
+	setAxiosClient(chainId, baseApiUrl);
+
+	const provider = new ethers.providers.JsonRpcProvider(
+		process.env[providerUrlEnvVar]
+	);
+
+	const usdt = await ERC20__factory.connect(
+		"0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
+		provider
+	);
+
+	const start_block = 150872800;
+	const end_block = 150872807;
+
+	for (let i = start_block; i < end_block; i++) {
+		const market_balance = await usdt.balanceOf(
+			"0x47563a2fA82200c0f652fd4688c71f10a2c8DAF3",
+			{
+				blockTag: i
+			}
+		);
+
+		console.log(market_balance.toString());
+
+		const vault_balance = await usdt.balanceOf(
+			"0xE37ae0A43d0f0e01a4AdB8605da2D2CD915E3906",
+			{
+				blockTag: i
+			}
+		);
+
+		console.log(vault_balance.toString());
+	}
+}
+
+if (require.main === module) {
+	main()
+		.then(() => {
+			console.log("Done");
+			process.exit(0);
+		})
+		.catch((error) => {
+			console.error(error);
+			process.exit(1);
+		});
+}
